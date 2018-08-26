d0e5c0390e6e2e3b9fc0ab7aa31d6aed673f9240
ashutosh7246 <ashutosh.shukla.7246@gmail.com>
2018-08-26 16:58:21 +0530

    added build folder

--- react/src/index.js
+++ react/src/index.js
@@ -6,9 +6,3 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-
-// https://github.com/ashutosh7246/ReactjsNodejsStarter
-// https://devcenter.heroku.com/articles/getting-started-with-nodejs#declare-app-dependencies
-// https://mlab.com/databases/reactjs-nodejs-starter/collections/userProfiles?saved=1535223068801&q=&s=&f=&pageSize=10&pageNum=0
-// https://www.linkedin.com/notifications/
-// https://reactnodedemo.herokuapp.com
